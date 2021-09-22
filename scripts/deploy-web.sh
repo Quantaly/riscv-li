b5a80b67ea6894fa25bff09669e1ad8013c572a1
Kai Page <kai@quantaly.net>
2021-09-22 12:59:52 -0600

    GitHub Actions: where the fuck is pkg

--- scripts/deploy-web.sh
+++ scripts/deploy-web.sh
@@ -10,6 +10,12 @@ cp -r .git $TEMPDIR
 cd $TEMPDIR
 git checkout -B gh-pages
 cp -Lr $OLDPWD/web/public/* .
+ls -R
+ls -R $OLDPWD/web
+if [ ! -d pkg ]; then
+    echo "pkg doesn't exist, fuck this shit"
+    exit 1
+fi
 git add .
 git commit -m "Dump"
 git push -fu origin gh-pages
