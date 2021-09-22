283863f2784c6739d68374f19eef529d98db5546
Kai Page <kai@quantaly.net>
2021-09-22 11:50:37 -0600

    GitHub Actions: maybe just use the existing tool??

--- scripts/deploy-web.sh
+++ scripts/deploy-web.sh
@@ -5,11 +5,4 @@ git config --global user.name $user_name
 git config --global user.email $user_email
 git remote set-url origin https://${github_token}@github.com/${repository}
 
-TEMPDIR=$(mktemp -d)
-cp -LR web/public $TEMPDIR
-git checkout -b gh-pages
-git rm -rf *
-cp -R $TEMPDIR/public/* .
-git add .
-git commit -m "Dump"
-git push -u origin gh-pages
+npx gh-pages -d web/public
