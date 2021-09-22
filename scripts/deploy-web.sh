076afb2eb9e9336b24d2cedad811b96a2ddc9ad9
Kai Page <kai@quantaly.net>
2021-09-22 12:20:46 -0600

    Github Actions: ugh plz work

--- scripts/deploy-web.sh
+++ scripts/deploy-web.sh
@@ -5,4 +5,4 @@ git config --global user.name $user_name
 git config --global user.email $user_email
 git remote set-url origin https://${github_token}@github.com/${repository}
 
-npx gh-pages -d web/public
+npx -- gh-pages -d web/public
