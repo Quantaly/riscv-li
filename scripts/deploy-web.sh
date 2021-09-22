e771f0cffa45c9bea7582eeca604df5c69f97598
Kai Page <kai@quantaly.net>
2021-09-22 12:49:34 -0600

    GitHub Actions: okay that didn't work

--- scripts/deploy-web.sh
+++ scripts/deploy-web.sh
@@ -5,4 +5,11 @@ git config --global user.name $user_name
 git config --global user.email $user_email
 git remote set-url origin https://${github_token}@github.com/${repository}
 
-npx -- gh-pages -d web/public
+TEMPDIR=$(mktemp -d)
+cp -r .git $TEMPDIR
+cd $TEMPDIR
+git checkout -B gh-pages
+cp -Lr $OLDPWD/web/public/* .
+git add .
+git commit -m "Dump"
+git push -u origin gh-pages
